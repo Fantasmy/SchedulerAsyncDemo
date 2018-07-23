d775135c8da1f4e08af1f89855ed83a52c961394
Jenny <fantazmit@gmail.com>
2018-07-23 15:22:59 +0200

    Finished

---
+++ SchedularPrac/Job.cs
@@ -0,0 +1,21 @@
+﻿using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace SchedularPrac
+{
+    public class Job : IJob  // implementa Ijob
+    {
+        public Task Execute(IJobExecutionContext context)  // método Execute
+        {
+            Task task = new Task(new Action(MyFuncAsync));
+            task.Start();
+            return task;
+        }
+
+        public void MyFuncAsync() { 
+            Form2 fm = new Form2();
+                fm.ShowDialog();
+        }
+    }
+}
---
+++ SchedularPrac/Scheduler.cs
@@ -0,0 +1,28 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+
+namespace SchedularPrac
+{
+  public  class Scheduler
+    {
+        public  async void Start(DateTime date)
+
+        {
+            StdSchedulerFactory sf = new StdSchedulerFactory();
+            IScheduler scheduler = await sf.GetScheduler();
+            await scheduler.Start();
+            scheduler.Start();
+            IJobDetail job = JobBuilder.Create<Job>().Build(); // utiliza otro patrón de diseño, el buider. Muy utilizado en .net core. Instancia clase job
+            ITrigger trigger = TriggerBuilder.Create()
+             .WithIdentity("IDGJob", "IDG") // envia entidad usuadio y password
+               .StartAt(date)  
+               .WithPriority(1)  // máxima con un 1
+               .Build(); // para crear el trigger. En Itrigger puedes ver todas las funcionalidades de trigger.
+
+            await scheduler.ScheduleJob(job, trigger);  // pasa 2 cosas, el job y el trigger
+
+        }
+
+    }
+}
